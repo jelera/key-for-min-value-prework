8e02e8d43dd43f040e00f7e6f2ac7c05c94706b9
Jose Elera <jelera@gmail.com>
2019-03-23 03:53:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
     nil
   else
 
-    smallest_value = 999999999 
+    smallest_value = 999999999
     
     name_hash.each do |name, value|
         smallest_value = value if value < smallest_value
